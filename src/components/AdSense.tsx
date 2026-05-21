7f4deeec65bf1647f60cd1d632a405a764436ff9
jhonyspicy <jhonyspicy@gmail.com>
2026-05-22 03:21:15 +0900

    AdSenseコンポーネント内の`ins`タグの記述を簡略化

--- src/components/AdSense.tsx
+++ src/components/AdSense.tsx
@@ -19,14 +19,12 @@ export default function AdSense() {
 
   return (
     <div className="w-full flex justify-center my-4">
-      <ins
-        className="adsbygoogle"
-        style={{ display: 'block', width: '100%', minWidth: '250px', minHeight: '250px' }}
-        data-ad-client="ca-pub-3080773555651810"
-        data-ad-slot="9350437098"
-        data-ad-format="auto"
-        data-full-width-responsive="true"
-      />
+      <ins className="adsbygoogle"
+           style="display:block"
+           data-ad-client="ca-pub-3080773555651810"
+           data-ad-slot="9350437098"
+           data-ad-format="auto"
+           data-full-width-responsive="true"></ins>
     </div>
   )
 }
