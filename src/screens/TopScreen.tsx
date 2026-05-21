a817c8bba78b4573dae7fd532cf5ac9c737e81ce
jhonyspicy <jhonyspicy@gmail.com>
2026-05-22 00:21:49 +0900

    トップ画面にAdSense広告を追加

--- src/screens/TopScreen.tsx
+++ src/screens/TopScreen.tsx
@@ -1,4 +1,4 @@
-import RangeTable from '../components/RangeTable'
+import AdSense from '../components/AdSense'
 
 type Props = {
   onStart: () => void
@@ -28,7 +28,7 @@ export default function TopScreen({ onStart, onAbout }: Props) {
         パワーナンバーとは？
       </button>
       <div className="w-full">
-        <RangeTable showPower />
+        <AdSense />
       </div>
     </div>
   )
